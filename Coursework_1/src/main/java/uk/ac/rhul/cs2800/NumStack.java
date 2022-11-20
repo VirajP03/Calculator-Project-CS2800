a9f112e00826c0e5087c48b08edf7e4bf233b0ed
zkac174 <zkac174@live.rhul.ac.uk>
2022-11-20 13:32:48 +0000

    Created a push() method in the NumStack class where it creates an entry and pushes it onto numStack, aprivate variable of type Stack

--- Coursework_1/src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ Coursework_1/src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -2,4 +2,12 @@ package uk.ac.rhul.cs2800;
 
 public class NumStack {
 
+  private Stack numStack = new Stack();
+
+  public void push(float f) {
+    Entry numEntry = new Entry(f);
+    numStack.push(numEntry);
+  }
+  
+
 }
--- Coursework_1/src/test/java/calculator/TestNumStack.java
+++ Coursework_1/src/test/java/calculator/TestNumStack.java
@@ -1,13 +1,32 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.ac.rhul.cs2800.Entry;
+import uk.ac.rhul.cs2800.NumStack;
+import uk.ac.rhul.cs2800.Stack;
+import uk.ac.rhul.cs2800.Symbol;
 
 class TestNumStack {
 
+  private NumStack numberStack;
+  private NumStack numStack;
+  
+  
+  @BeforeEach
+  public void setup() {
+    numberStack = new NumStack();
+  }
+  
   @Test
   void test() {
-    NumStack numberStack = new NumStack();
+    numStack = new NumStack();
+  }
+  
+  @Test
+  void testPush() {
+    numberStack.push(7.0f);
   }
 
 }
