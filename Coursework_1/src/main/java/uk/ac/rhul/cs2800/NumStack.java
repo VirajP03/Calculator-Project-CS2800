e6fc3410b00906a5274bbbab7df8bd247cc802c8
zkac174 <zkac174@live.rhul.ac.uk>
2022-11-22 15:05:42 +0000

    I created the push() method in the NumStack class which creates a Entry object containing the float value entered and pushes it to the top of the stack, the push() method has a basic test as push() from the Stack class works

--- Coursework_1/src/main/java/uk/ac/rhul/cs2800/NumStack.java
+++ Coursework_1/src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -1,13 +1,30 @@
 package uk.ac.rhul.cs2800;
 
+import java.util.EmptyStackException;
+
+/**
+ * @author virajpatel
+ *
+ */
 public class NumStack {
 
-  private Stack numStack = new Stack();
+  private Stack numStack;
 
+  /**
+   * This method is used to create the NumStack object and initialises a Stack object.
+   * 
+   */
+  public NumStack() {
+    numStack = new Stack();
+  }
+
+  /**
+   * This method is used to push a float value to the top of numStack.
+   *
+   * @param f is the float value that will be pushed to the top of the stacks.
+   */
   public void push(float f) {
     Entry numEntry = new Entry(f);
     numStack.push(numEntry);
   }
-  
-
 }
--- Coursework_1/src/test/java/calculator/TestNumStack.java
+++ Coursework_1/src/test/java/calculator/TestNumStack.java
@@ -1,29 +1,28 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import uk.ac.rhul.cs2800.Entry;
 import uk.ac.rhul.cs2800.NumStack;
-import uk.ac.rhul.cs2800.Stack;
-import uk.ac.rhul.cs2800.Symbol;
 
 class TestNumStack {
 
   private NumStack numberStack;
   private NumStack numStack;
-  
-  
+
+
   @BeforeEach
   public void setup() {
     numberStack = new NumStack();
   }
-  
+
   @Test
   void test() {
     numStack = new NumStack();
   }
-  
+
   @Test
   void testPush() {
     numberStack.push(7.0f);
