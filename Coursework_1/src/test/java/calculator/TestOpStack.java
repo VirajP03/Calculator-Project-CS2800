e61734d9e8180a20dd7d80775599bf2b8c48665f
zkac174 <zkac174@live.rhul.ac.uk>
2022-11-30 12:56:12 +0000

    I created a test to initialise a OpStack object called opStack which failed the test. In order to pass the test I create the OpStack class in main

---
+++ Coursework_1/src/test/java/calculator/TestOpStack.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestOpStack {
+
+  @Test
+  void test() {
+    OpStack opStack = new OpStack();
+  }
+
+}
