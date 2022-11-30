683542a5aa594f4c7898f8602149485f283108a3
zkac174 <zkac174@live.rhul.ac.uk>
2022-11-30 13:19:49 +0000

    I created a test to push a symbol (i.e. operator) to the stack, this initially failed. To pass the tesst I created the push method in OpStack. This resulted me in creating a private attribute called opStack of type Stack to push to, I had to create a constructor to then initialise the opStack object. This resulted in the test passing.

--- Coursework_1/src/main/java/uk/ac/rhul/cs2800/OpStack.java
+++ Coursework_1/src/main/java/uk/ac/rhul/cs2800/OpStack.java
@@ -1,5 +1,34 @@
 package uk.ac.rhul.cs2800;
 
+/**
+ * This class contains the operations used to maintain a stack specifically for Symbol values,
+ * making it easier to manage the operations of a stack with Symbol objects. The methods within
+ * allow you to add and remove a symbol, check the top value of the stack and check if the stack is
+ * empty.
+ *
+ * @author Viraj Patel (zkac174)
+ *
+ */
 public class OpStack {
 
+  private Stack opStack;
+
+  /**
+   * The constructor is used to initialise the opStack object as a Stack which is empty.
+   * 
+   */
+  public OpStack() {
+    opStack = new Stack();
+  }
+
+  /**
+   * This method is used to push a Symbol value to the top of opStack.
+   *
+   * @param operation is the symbol that will be used to create an Entry which will be pushed to the
+   *        top of the Stack.
+   */
+  public void push(Symbol operation) {
+    Entry opEntry = new Entry(operation);
+    opStack.push(opEntry);
+  }
 }
--- Coursework_1/src/test/java/calculator/TestOpStack.java
+++ Coursework_1/src/test/java/calculator/TestOpStack.java
@@ -1,13 +1,27 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.ac.rhul.cs2800.OpStack;
+import uk.ac.rhul.cs2800.Symbol;
 
 class TestOpStack {
+  
+  OpStack operatorStack;
+
+  @BeforeEach
+  void setup() {
+    operatorStack = new OpStack();
+  }
 
   @Test
   void test() {
     OpStack opStack = new OpStack();
+  }
+
+  @Test
+  void testPushToOpStack() {
+    operatorStack.push(Symbol.PLUS);
   }
 
 }
