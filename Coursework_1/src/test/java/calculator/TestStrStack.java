ac3baa4e61ed99250ae013aaaa7ab8e7f6ca7c32
zkac174 <zkac174@live.rhul.ac.uk>
2022-12-02 22:35:29 +0000

    I created a new test case called TestStrStack to write tests for a class called StrStack. My first test was creating a StrStack, which failed. To pass the test, I created a new class called StrStack in main.

---
+++ Coursework_1/src/test/java/calculator/TestStrStack.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import uk.ac.rhul.cs2800.StrStack;
+
+class TestStrStack {
+
+  @Test
+  void test() {
+    StrStack strStack = new StrStack();
+  }
+
+}
