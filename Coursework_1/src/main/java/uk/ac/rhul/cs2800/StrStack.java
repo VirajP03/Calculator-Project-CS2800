818ce82897665bc12fbec9f6147faaad8b579c20
zkac174 <zkac174@live.rhul.ac.uk>
2022-12-03 16:49:15 +0000

    I created a test to test the push method, the test pushed the string Test1 to a StrStack object and then checked the size of the stack was 1. This failed initially because the push method didn't exist. To pass the test, I created the push method that took a string as a parameter which would be the content of the Entry object, and then I pushed the entry to the stack. I then created a private attribute of a Stack called strStack which would be the stack operated on, in addition to this, I added a constructor to initialise strStack. Finally, I created the size() method, which returns the size of strStack.

--- Coursework_1/src/main/java/uk/ac/rhul/cs2800/StrStack.java
+++ Coursework_1/src/main/java/uk/ac/rhul/cs2800/StrStack.java
@@ -1,5 +1,43 @@
 package uk.ac.rhul.cs2800;
 
+/**
+ * This class contains the operations used to maintain a stack specifically for Strings. Therefore
+ * making it easier to manage and implement a stack for a String element. The methods within allow
+ * you to add and remove a String, check the top value of the stack and check if the stack is empty.
+ *
+ * @author Viraj Patel (zkac174)
+ *
+ */
 public class StrStack {
+
+  private Stack strStack;
+
+  /**
+   * The constructor is used to initialise the strStack object as a Stack which is initially empty.
+   * 
+   */
+  public StrStack() {
+    strStack = new Stack();
+  }
+
+  /**
+   * This method is used to create an entry containing the string which will then be placed at the
+   * top of the stack.
+   *
+   * @param string used in the entry to be pushed to the stack.
+   */
+  public void push(String string) {
+    Entry strEntry = new Entry(string);
+    strStack.push(strEntry);
+  }
+
+  /**
+   * This method is used to get the size of the String stack.
+   *
+   * @return the size of the String stack.
+   */
+  public int size() {
+    return strStack.size();
+  }
 
 }
--- Coursework_1/src/test/java/calculator/TestStrStack.java
+++ Coursework_1/src/test/java/calculator/TestStrStack.java
@@ -1,14 +1,29 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.rhul.cs2800.StrStack;
 
 class TestStrStack {
 
+  StrStack stringStack;
+
+  @BeforeEach
+  void setup() {
+    stringStack = new StrStack();
+  }
+  
   @Test
   void test() {
     StrStack strStack = new StrStack();
   }
-
+  
+  @Test
+  void testPush() {
+    stringStack.push("Test1");
+    assertEquals(stringStack.size(), 1);
+  }
+ 
 }
