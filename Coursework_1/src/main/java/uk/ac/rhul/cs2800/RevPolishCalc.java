844d32fa538ffa9f11573c9d7e12e73c4598bdf4
zkac174 <zkac174@live.rhul.ac.uk>
2022-12-05 22:01:43 +0000

    I created a new test and the reason behind this was to test the evaluate method, this method will evaluate a string and return a float, in my test, I passed the string '3 4 +' to the method and the expected return value should be 7.0f. Initially this test failed. To pass the test, I created the evaluate method that took a string as a parameter and return 7.0f.

--- Coursework_1/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ Coursework_1/src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -2,4 +2,8 @@ package uk.ac.rhul.cs2800;
 
 public class RevPolishCalc {
 
+  public Float evaluate(String string) {
+    return 7.0f;
+  }
+
 }
--- Coursework_1/src/test/java/calculator/TestRevPolishCalc.java
+++ Coursework_1/src/test/java/calculator/TestRevPolishCalc.java
@@ -1,14 +1,29 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.rhul.cs2800.RevPolishCalc;
 
 class TestRevPolishCalc {
 
+  RevPolishCalc rc;
+  
+  @BeforeEach
+  void setup() {
+    rc = new RevPolishCalc(); 
+  }
+  
   @Test
   void test() {
     RevPolishCalc revCalc = new RevPolishCalc();
   }
+  
+  @Test
+  void testEvaluate() {
+    assertEquals(rc.evaluate("3 4 +"), 7.0f);
+  }
+  
 
 }
