9d6072ac7efec4d9e7a00c545ee3ed0c97a4b50f
zkac174 <zkac174@live.rhul.ac.uk>
2022-12-05 20:56:29 +0000

    To start the creation of RevPolish calc I created a test which instantiates a RevPolishCalc object. It initially failed as the RevPolishCalc class doesn't exist. Creating the class passed the test

---
+++ Coursework_1/src/test/java/calculator/TestRevPolishCalc.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import uk.ac.rhul.cs2800.RevPolishCalc;
+
+class TestRevPolishCalc {
+
+  @Test
+  void test() {
+    RevPolishCalc revCalc = new RevPolishCalc();
+  }
+
+}
