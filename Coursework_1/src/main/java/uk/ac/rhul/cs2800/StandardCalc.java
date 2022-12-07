20656ffb5468dc10cc6d18b31dbcc93436a35c0d
zkac174 <zkac174@live.rhul.ac.uk>
2022-12-07 21:36:40 +0000

    I created a test to start the creation of the infix calculator. The test consists of a object of type StandardCalc being created. This test failed, thus I created the StandardCalc class in main. This passed the test.

---
+++ Coursework_1/src/main/java/uk/ac/rhul/cs2800/StandardCalc.java
@@ -0,0 +1,12 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * This class will implement the infix calculator where it uses the shunting yard algorithm to
+ * rearrange the expression given into reverse polish notation.
+ *
+ * @author Viraj Patel (zkac174)
+ *
+ */
+public class StandardCalc {
+
+}
---
+++ Coursework_1/src/test/java/calculator/TestStandardCalc.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import uk.ac.rhul.cs2800.StandardCalc;
+
+class TestStandardCalc {
+
+  @Test
+  void test() {
+    StandardCalc sc = new StandardCalc();
+  }
+
+}
